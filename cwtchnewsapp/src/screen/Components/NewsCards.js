a9d6ef685fa1d371e4e5dd140f6aad3c02607484
vicky <srivicky2000@gmail.com>
2021-07-05 09:08:47 +0530

    Added Pagination

--- cwtchnewsapp/src/screen/Components/NewsCards.js
+++ cwtchnewsapp/src/screen/Components/NewsCards.js
@@ -728,13 +728,13 @@ const noPressed = () => {
       <Pagination
       dotsLength={ARTICLES.news.allHeadings.length}
       activeDotIndex={indexAt}
-      containerStyle={{ backgroundColor: 'rgba(0, 0, 0, 0.75)' }}
+      containerStyle={{ backgroundColor: '#fff' }}
       dotStyle={{
           width: 10,
           height: 10,
           borderRadius: 5,
           marginHorizontal: 8,
-          backgroundColor: 'rgba(255, 255, 255, 0.92)'
+          backgroundColor: '#242B2E'
       }}
       inactiveDotStyle={{
           // Define styles for inactive dots here
